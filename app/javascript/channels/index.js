253394c3336a56e533f3e60da6f1f7c9110a1f29
Jerico Lopez <jerico.lopez1993@gmail.com>
2024-01-25 00:35:41 +0800

    generate cable and controller for dog search

---
+++ app/javascript/channels/index.js
@@ -0,0 +1,2 @@
+// Import all the channels to be used by Action Cable
+import "channels/dog_search_channel"
---
+++ test/channels/dog_search_channel_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class DogSearchChannelTest < ActionCable::Channel::TestCase
+  # test "subscribes" do
+  #   subscribe
+  #   assert subscription.confirmed?
+  # end
+end
